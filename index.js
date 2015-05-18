be0b0773893b93267053b6e7d0362247385c4d7d
Sam Delesque <sdelesque@domanistudios.com>
2015-05-18 14:47:34 -0400

    globalized module to use anywhere with cli

--- index.js
+++ index.js
@@ -2,35 +2,66 @@ var juice = require('juice2'),
 	fs = require('fs'),
 	http = require('http'),
 	watch = require('node-watch'),
-	rootpath = process.cwd(),
-	templatesPath = rootpath+"/templates/",
 	disregardFiles = [".DS_Store",".hide",".git",".deleteme","css"],
 	routes = {},
-	port = 3000
+	port = 3000,
+	basePath = process.cwd()+"/"
 
 
-function renderTemplate(path){
-	juice(templatesPath+path, function(err, html) {
+function renderTemplate(inputdir,outputdir,path){
+	juice(inputdir+path, function(err, html) {
 		if(err){console.log(path,"Path not found");}
 		routes["/"+path] = html
-		console.log(path,"ready.")
+		fs.writeFile(outputdir+path, html, function(err){
+			if(err){
+				console.error("Failed to write",outputdir+path,"!");
+			}
+			else{
+				console.log(outputdir+path,"was rendered.")
+			}
+		})
+	})
+}
+function parseInput(inputdir,outputdir,dir){
+	fs.readdir(inputdir+dir,function(err,templates){
+		if(err){ console.error("Could not open templates dir!",err); return;}
+
+		templates.forEach(function(path){
+			if(disregardFiles.indexOf(path) == -1){
+				renderTemplate(inputdir,outputdir,path)
+
+				// watch folder for changes
+				watch(dir+path, function(filename) {
+					renderTemplate(inputdir,outputdir,path)
+				});
+			}
+		});
 	})
 }
 
-fs.readdir(templatesPath,function(err,templates){
-	if(err){ console.error("Could not open templates dir!",err); return;}
+if(!process.argv[2] || !process.argv[3]){
+	console.error("Please specify a valid input and output dir (node ds-email-builder /input/path /output/path)")
+	process.kill()
+}
+else{
+	var inputPath = process.argv[2].replace(/\/+$/, "")+"/",
+		outputPath = process.argv[3].replace(/\/+$/, "")+"/"
 
-	templates.forEach(function(path){
-		if(disregardFiles.indexOf(path) == -1){
-			renderTemplate(path)
+	console.log("Starting email processing",inputPath,outputPath);
 
-			// watch folder for changes
-			watch(templatesPath+path, function(filename) {
-				renderTemplate(path)
-			});
+	fs.lstat(basePath+inputPath, function(err, stats) {
+		if (!err && stats.isDirectory()){
+	    	parseInput(basePath+inputPath,basePath+outputPath,"")
 		}
-	});
-})
+	})
+	fs.lstat(basePath+outputPath, function(err, stats) {
+		if (!err && stats.isDirectory()) {
+			console.error("Invalid output directory!",basePath+outputPath)
+			process.kill()
+		}
+	})
+}
+
 
 http.createServer(function(req,res){
 	console.log("Requesting",req.url)
