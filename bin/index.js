f33541c6afbe6b8385ed5c117756d23867105fb2
Sam Delesque <sdelesque@domanistudios.com>
2015-05-19 11:03:44 -0400

    objectified module

--- bin/index.js
+++ bin/index.js
@@ -1,94 +1,126 @@
-#! /usr/bin/env node
+// #! /usr/bin/env node
 var juice = require('juice2'),
 	fs = require('fs'),
 	http = require('http'),
-	watch = require('node-watch'),
+	staticServer = require('node-static'),
+	Watch = require('node-watch'),
 	disregardFiles = [".DS_Store",".hide",".git",".deleteme","css","less","scss","sass"],
 	routes = {},
 	port = 3729,
-	basePath = process.cwd()+"/"
+	basePath = process.cwd()+"/",
+	usage = ["watch","create"]
 
-console.log("\x1B[36m");
-console.log("");
-console.log("           DS EMAIL BUILDER");
-console.log("");
-console.log("\x1B[0m");
+var Emailizer = {
+	settings: {
+		inputdir: "",
+		outputdir: ""
+	},
+	staticServer: {},
 
-function renderTemplate(inputdir,outputdir,path){
-	juice(inputdir+path, function(err, html) {
-		if(err){console.log(path,"Path not found");}
-		routes["/"+path] = html
-		fs.writeFile(outputdir+path, html, function(err){
-			if(err){
-				console.error("Failed to write",outputdir+path,"!");
-			}
-			else{
-				console.log(outputdir+path,"was rendered.")
-			}
+	init: function(){
+		var s = this
+		console.log("\x1B[36m");
+		console.log("");
+		console.log("EMAILIZER");
+		console.log("");
+		console.log("\x1B[0m");
+	},
+
+	renderTemplate: function(path){
+		var s = this
+		juice(s.settings.inputdir+path, function(err, html) {
+			if(err){console.log(path,"Path not found");}
+			// routes["/"+path] = html
+			fs.writeFile(s.settings.outputdir+path, html, function(err){
+				if(err){
+					console.error("Failed to write",s.settings.outputdir+path,"!");
+				}
+				else{
+					console.log(s.settings.outputdir+path,"was rendered.")
+				}
+			})
 		})
-	})
+	},
+	parseInput: function(){
+		var s = this
+		fs.readdir(s.settings.inputdir,function(err,templates){
+			if(err){ console.error("Could not open templates dir!",err); return;}
+
+			templates.forEach(function(path){
+				if(disregardFiles.indexOf(path) == -1){
+					s.renderTemplate(path)
+
+					// watch folder for changes
+					Watch(s.settings.inputdir+path, function(filename) {
+						s.renderTemplate(path)
+					})
+				}
+			})
+		})
+	},
+	serveRender: function(){
+		var s = this
+		s.staticServer = new static.Server(s.settings.outputdir)
+
+		http.createServer(function (req, res) {
+			req.addListener('end', function () {
+				s.staticServer.serve(req, res);
+			}).resume()
+		}).listen(port)
+
+		console.log("App files are now served on port "+port);
+	},
+
+
+	watch: function(inputdir,outputdir){
+		var s = this
+		s.settings.inputdir = inputdir
+		s.settings.outputdir = outputdir
+
+		s.parseInput()
+	}
 }
-function parseInput(inputdir,outputdir,dir){
-	fs.readdir(inputdir+dir,function(err,templates){
-		if(err){ console.error("Could not open templates dir!",err); return;}
 
-		templates.forEach(function(path){
-			if(disregardFiles.indexOf(path) == -1){
-				renderTemplate(inputdir,outputdir,path)
-
-				// watch folder for changes
-				watch(inputdir+dir+path, function(filename) {
-					renderTemplate(inputdir,outputdir,path)
-				});
-			}
-		});
-	})
-}
-
-if(!process.argv[2] || !process.argv[3]){
-	console.error("Please specify a valid input and output dir (node ds-email-builder /input/path /output/path)")
+if(!process.argv[2] || process.argv[2] == "help" || usage.indexOf(process.argv[2]) == -1){
+	console.log("");
+	console.log("usage: emailizer [create NAME] [watch ./SOURCE ./DEST]");
+	console.log("");
 	process.kill()
 }
 else{
-	var inputPath = process.argv[2].replace(/\/+$/, "")+"/",
-		outputPath = process.argv[3].replace(/\/+$/, "")+"/"
 
-	if(inputPath.substr(0,1) != "/"){
-		inputPath = basePath + inputPath;
-	}
-	if(outputPath.substr(0,1) != "/"){
-		outputPath = basePath + outputPath;
-	}
+	Emailizer.init();
 
-	console.log("Starting email processing",inputPath,outputPath);
-
-	fs.lstat(inputPath, function(err, stats) {
-		if (!err && stats.isDirectory()){
-	    	parseInput(inputPath,outputPath,"")
-		}
-	})
-	fs.lstat(outputPath, function(err, stats) {
-		if (err || !stats.isDirectory()) {
-			console.error("Invalid output directory!",outputPath)
+	if(process.argv[2] == "watch"){
+		if(!process.argv[3] || !process.argv[4]){
+			console.error("Please specify a valid SOURCE and DEST directory")
 			process.kill()
 		}
-	})
+		else{
+			var source = process.argv[3].replace(/\/+$/, "")+"/",
+				dest = process.argv[4].replace(/\/+$/, "")+"/"
+
+			if(source.substr(0,1) != "/"){
+				source = basePath + source;
+			}
+			if(dest.substr(0,1) != "/"){
+				dest = basePath + dest;
+			}
+
+			fs.lstat(source, function(err, stats) {
+				if (!err && stats.isDirectory()){
+			    	Emailizer.watch(source,dest)
+				}
+			})
+			fs.lstat(dest, function(err, stats) {
+				if (err || !stats.isDirectory()) {
+					console.error("Invalid output directory!",dest)
+					process.kill()
+				}
+			})
+		}
+	}
+	else if(process.argv[2] == "create"){
+		console.log("Create function is under construction....");
+	}
 }
-
-
-http.createServer(function(req,res){
-	console.log("Requesting",req.url)
-	if(!routes[req.url]){
-      res.writeHead(404, {"Content-Type": "text/plain"})
-      res.write("404 Not Found\n")
-      res.end()
-      return
-	}
-	else{
-      res.writeHead(200)
-      res.write(routes[req.url])
-      res.end()
-	}
-}).listen(port)
-
-console.log("Templates served on http://localhost:"+port+"/")
